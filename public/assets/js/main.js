07d2b0db47fbd58afdd71b2c5fad3e8b38bbf568
lupeMorales <morales.lupe@gmail.com>
2023-06-26 19:43:02 +0200

    update button appearance

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -1,5 +1,12 @@
-'use strict';
+"use strict";
 
-console.log('>> Ready :)');
+const taxBase = document.querySelector(".js-tax-base");
+const iva = document.querySelector(".js-iva");
+const ivaResult = document.querySelector(".js-iva-result");
+const irpf = document.querySelector(".js-irpf");
+const irpfResult = document.querySelector(".js-irpf-result");
+const price = document.querySelector(".js-price");
+const totalInvoice = document.querySelector(".js-total");
+const totalToReceive = document.querySelector(".js-total-to-receive");
 
 //# sourceMappingURL=main.js.map
--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,10 @@
-'use strict';
+"use strict";
 
-console.log('>> Ready :)');
+const taxBase = document.querySelector(".js-tax-base");
+const iva = document.querySelector(".js-iva");
+const ivaResult = document.querySelector(".js-iva-result");
+const irpf = document.querySelector(".js-irpf");
+const irpfResult = document.querySelector(".js-irpf-result");
+const price = document.querySelector(".js-price");
+const totalInvoice = document.querySelector(".js-total");
+const totalToReceive = document.querySelector(".js-total-to-receive");
